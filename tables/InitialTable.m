93163a6432d92c014f67819bd817e37938118171
hanselitz <hansel.itz@gmail.com>
2014-10-04 13:49:35 -0500

    New ones

---
+++ tables/InitialTable.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  tables
+//
+//  Created by Hansel Itz on 04/10/14.
+//  Copyright (c) 2014 Hansel Itz. All rights reserved.
+//
+
+#import "InitialTable.h"
+
+@interface InitialTable ()
+
+@end
+
+@implementation InitialTable
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"No te agüites, Miguel.");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
